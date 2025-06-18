97c1eeff5eb0413ce477c17f98afcd31714f47c3
srivarthan12 <sri2112045@gmail.com>
2025-06-18 20:59:04 +0530

    project completed

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
 if(process.env.NODE_ENV==="PRODUCTION"){
-    app.use(express.static(Path.join(__dirname,"../frontend/dist")))
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
